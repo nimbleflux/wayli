d1ebe3d1288d91262c9fbc7205cfe9e01a57d2ae
Bart Hazen <bart@hazen.nu>
2026-09-26 14:17:20 +0200

    style: prettier

--- web/src/lib/services/external/valhalla.service.ts
+++ web/src/lib/services/external/valhalla.service.ts
@@ -47,7 +47,8 @@ export async function getValhallaEndpoint(fluxbase?: FluxbaseClient): Promise<st
 	if (fluxbase) {
 		try {
 			const { data, error } = await fluxbase
-				.schema('app').from('settings')
+				.schema('app')
+				.from('settings')
 				.select('value')
 				.eq('key', 'wayli.valhalla_endpoint')
 				.single();
